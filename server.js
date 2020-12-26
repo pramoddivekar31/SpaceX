4b83396ca41ded2951b511b678f22c8e4fe67dc3
PramodGorakh Divekar <pramodgorakh.d@ad.infosys.com>
2020-12-26 19:13:44 +0530

    Fix : server.js dist path

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/space-x'));
+app.use(express.static('./dist/SpaceX'));
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {
-    root: 'dist/space-x/'
+    root: 'dist/SpaceX/'
   }),
 );
 
